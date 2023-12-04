d0e41c7aab31fa26ddeaef043dce494a2a8b42c6
WTTien <wtiantien@hotmail.com>
2023-12-04 23:25:01 +0000

    Modify logic to make it similar to touchpad

--- main.py
+++ main.py
@@ -8,21 +8,33 @@ class MyWidget(Widget):
 	def on_touch_down(self, touch):
 		touch.ud['line'] = Line(points=(touch.x, touch.y))
 		self.canvas.add(touch.ud['line'])
+		touch.ud['prev_x'] = touch.x
+		touch.ud['prev_y'] = touch.y
 		self.send_data(touch.x, touch.y, 1)
 
 	def on_touch_move(self, touch):
 		touch.ud['line'].points += [touch.x, touch.y]
-		self.send_data(touch.x, touch.y, 2)
+		x = touch.x - touch.ud['prev_x']
+		y = touch.y - touch.ud['prev_y']
+		touch.ud['prev_x'] = touch.x
+		touch.ud['prev_y'] = touch.y
+		self.send_data(x, y, 2)
+
+	def on_touch_up(self, touch):
+		self.canvas.remove(touch.ud['line'])
+		del touch.ud['line']
+		self.canvas.clear()
 
 	def send_data(self, x, y, mode):
 		serverAddress = ('192.168.137.1', 8080)
-		y = 1080 - y
+		y = -y
 		message = f'{x} , {y} ,{mode} '
 		bytesToSend = message.encode('utf-8')
 
 		try:
 			clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 			clientSocket.sendto(bytesToSend, serverAddress)
+			print(message)
 
 		except socket.error as e:
 			print(f"Error: {e}")
