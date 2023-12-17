594e38157922df5f63c450d9cf8885eabd859641
WTTien <wtiantien@hotmail.com>
2023-12-17 03:00:37 +0000

    time.sleep(0.15) for send_data to have laptop receive message niclier

--- main.py
+++ main.py
@@ -11,33 +11,21 @@ class MyWidget(Widget):
 	def on_touch_down(self, touch):
 		touch.ud['line'] = Line(points=(touch.x, touch.y))
 		self.canvas.add(touch.ud['line'])
-		touch.ud['prev_x'] = touch.x
-		touch.ud['prev_y'] = touch.y
-		touch.ud['decide'] = 0
-
 		self.send_data(touch.x, touch.y, 1)
 
 	def on_touch_move(self, touch):
 		touch.ud['line'].points += [touch.x, touch.y]
-
-#		x = touch.x - touch.ud['prev_x']
-#		y = touch.y - touch.ud['prev_y']
-#		touch.ud['prev_x'] = touch.x
-#		touch.ud['prev_y'] = touch.y
-
 		self.send_data(touch.x, touch.y, 2)
 
 	def on_touch_up(self, touch):
 		self.send_data(touch.x, touch.y , 3)
-
 		self.canvas.remove(touch.ud['line'])
 		del touch.ud['line']
 		self.canvas.clear()
 
 	def send_data(self, x, y, mode):
+		time.sleep(0.15)
 		serverAddress = ('192.168.137.1', 8080)
-#		x = x
-#		y = -y
 		message = f'{x} , {y} ,{mode} '
 		bytesToSend = message.encode('utf-8')
 
@@ -51,7 +39,6 @@ class MyWidget(Widget):
 
 
 class MyApp(App):
-	Config.set('input','multitouch_double_tap_time', '0.2')
 	def build(self):
 		return MyWidget()
 
